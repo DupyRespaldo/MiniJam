c42c137fe37409b1befc1c4da394ff2e6d07eb69
ArminPosadas <armin.posadas@uniat.edu.mx>
2025-11-04 18:47:00 -0600

    Revert "Merge branch 'main' into Armin"
    This reverts commit 0a9bb32c2b8162396f71817b58cd74e10f73e03f, reversing
    changes made to 55cd65139107b4ea579bac78c4ac436c1bf8a6ce.

--- Source/minijam/Private/Pilar.cpp
+++ Source/minijam/Private/Pilar.cpp
@@ -1,92 +1,54 @@
 #include "Pilar.h"
-#include "Components/BoxComponent.h"
 #include "Net/UnrealNetwork.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
 
 APilar::APilar()
 {
-	PrimaryActorTick.bCanEverTick = true;
 	bReplicates = true;
-
-	PilarMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PilarMesh"));
-	RootComponent = PilarMesh;
-
-	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
-	TriggerBox->SetupAttachment(RootComponent);
-	TriggerBox->SetBoxExtent(FVector(100.f));
-	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
-
-	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &APilar::OnOverlapBegin);
-	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &APilar::OnOverlapEnd);
-
-	MoveSpeed = 1.f;
-	LowerDistance = 200.f;
-	bIsLowering = false;
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 void APilar::BeginPlay()
 {
 	Super::BeginPlay();
-
 	InitialLocation = GetActorLocation();
-	CurrentLocation = InitialLocation;
 }
 
 void APilar::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority()) 
+	if (!HasAuthority()) return;
+
+	FVector NewLocation = GetActorLocation();
+
+	if (bPlayerOnTop)
 	{
-		HandleMovement();
+		NewLocation.Z -= Speed * 20 * DeltaTime;
 	}
-	else 
+	else
 	{
-		SetActorLocation(CurrentLocation);
+		float DeltaZ = FMath::Sin(GetWorld()->GetTimeSeconds() * Speed) * OscillationHeight;
+		NewLocation.Z = InitialLocation.Z + DeltaZ;
 	}
-}
 
-void APilar::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-                            const FHitResult& SweepResult)
-{
-	if (HasAuthority() && OtherActor && OtherActor != this)
-	{
-		bIsLowering = true;
-	}
-}
-
-void APilar::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	if (HasAuthority() && OtherActor && OtherActor != this)
-	{
-		bIsLowering = false;
-	}
-}
-
-void APilar::HandleMovement()
-{
-	FVector TargetLocation = InitialLocation;
-
-	if (bIsLowering)
-		TargetLocation.Z -= LowerDistance;
-
-	FVector NewLocation = FMath::VInterpTo(CurrentLocation, TargetLocation, GetWorld()->GetDeltaSeconds(), MoveSpeed);
 	SetActorLocation(NewLocation);
-
-	CurrentLocation = NewLocation; 
 }
 
-void APilar::OnRep_PilarLocation()
+void APilar::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,const FHitResult& SweepResult)
 {
-	SetActorLocation(CurrentLocation);
+	bPlayerOnTop = true;
+}
+
+void APilar::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	bPlayerOnTop = false;
 }
 
 void APilar::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
-	DOREPLIFETIME(APilar, InitialLocation);
-	DOREPLIFETIME(APilar, CurrentLocation);
-	DOREPLIFETIME(APilar, bIsLowering);
+	DOREPLIFETIME(APilar, OscillationHeight);
+	DOREPLIFETIME(APilar, Speed);
 }
--- Source/minijam/Public/Pilar.h
+++ Source/minijam/Public/Pilar.h
@@ -2,58 +2,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "CollisionHandler.h"
 #include "Pilar.generated.h"
 
 UCLASS()
-class MINIJAM_API APilar : public AActor
+class MINIJAM_API APilar : public ACollisionHandler
 {
 	GENERATED_BODY()
 
 public:
 	APilar();
-
-protected:
 	virtual void BeginPlay() override;
-
-	UPROPERTY(VisibleAnywhere, Category = "Pilar")
-	class UBoxComponent* TriggerBox;
-	
-	UPROPERTY(VisibleAnywhere, Category = "Pilar")
-	class UStaticMeshComponent* PilarMesh;
-	
-	UPROPERTY(Replicated)
-	FVector InitialLocation;
-	
-	UPROPERTY(ReplicatedUsing = OnRep_PilarLocation)
-	FVector CurrentLocation;
-	
-	UPROPERTY(EditAnywhere, Category = "Pilar")
-	float MoveSpeed;
-	
-	UPROPERTY(EditAnywhere, Category = "Pilar")
-	float LowerDistance;
-	
-	UPROPERTY(Replicated)
-	bool bIsLowering;
-
-	FTimerHandle MovementTimer;
-
-	UFUNCTION()
-	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-						const FHitResult& SweepResult);
-
-	UFUNCTION()
-	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-					  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-	
-	void HandleMovement();
-
-	UFUNCTION()
-	void OnRep_PilarLocation();
-
-public:
-	virtual void Tick(float DeltaTime) override;
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+protected:
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,const FHitResult& SweepResult) override;
+
+	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "Pilar")
+	float OscillationHeight = 100.f;
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "Pilar")
+	float Speed = 2.0f;
+
+	bool bPlayerOnTop = false;
+	FVector InitialLocation;
+
 };
